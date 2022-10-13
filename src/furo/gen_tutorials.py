76430fedc097cd345cafdecbe5e2fbf4030b96fe
Manuel Goulão <msilvagoulao@gmail.com>
2022-10-13 16:31:37 +0100

    add a tutorial generation script

---
+++ src/furo/gen_tutorials.py
@@ -0,0 +1,23 @@
+import os
+from sphinx_gallery import gen_rst
+from sphinx_gallery.gen_gallery import DEFAULT_GALLERY_CONF
+
+def generate(source_path, dest_path):
+    for filename in os.listdir(source_path):
+        if os.path.splitext(filename)[1] != ".py":
+            continue
+
+        gen_rst.EXAMPLE_HEADER = ""
+        gen_rst.SPHX_GLR_SIG = ""
+
+        file_path = os.path.join(source_path, filename)
+        gallery_config = DEFAULT_GALLERY_CONF
+        gallery_config["lang"] = "python"
+        gallery_config["src_dir"] = file_path
+        gallery_config["titles"] = {}
+        gallery_config["titles"][file_path] = ""
+        gallery_config["memory_base"] = 0.0
+        gallery_config["min_reported_time"] = float("inf")
+        gallery_config["exclude_implicit_doc_regex"] = True
+
+        gen_rst.generate_file_rst(filename, dest_path, source_path, gallery_config)
