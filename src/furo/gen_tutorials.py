1a4d5283de97c1930ef86108f2224cc7b804aa53
Manuel Goulão <msilvagoulao@gmail.com>
2022-10-18 23:11:57 +0100

    updates to sphinx gallery

--- src/furo/gen_tutorials.py
+++ src/furo/gen_tutorials.py
@@ -1,6 +1,10 @@
 import os
 from sphinx_gallery import gen_rst
 from sphinx_gallery.gen_gallery import DEFAULT_GALLERY_CONF
+from sphinx_gallery.scrapers import matplotlib_scraper
+
+def call_memory(func):
+    return 0., func()
 
 def generate(source_path):
     dest_path = source_path
@@ -17,8 +21,12 @@ def generate(source_path):
         gallery_config["titles"] = {}
         gallery_config["titles"][file_path] = ""
         gallery_config["memory_base"] = 0.0
+        gallery_config["call_memory"] = call_memory
         gallery_config["min_reported_time"] = float("inf")
         gallery_config["exclude_implicit_doc_regex"] = True
         gallery_config["show_signature"] = False
+        gallery_config["filename_pattern"] = ".*"
+        gallery_config["reset_modules"] = ()
+        gallery_config['image_scrapers'] = tuple([matplotlib_scraper])
 
         gen_rst.generate_file_rst(filename, dest_path, source_path, gallery_config)
